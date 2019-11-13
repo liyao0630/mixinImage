66015a8946903fd9e638c1612210ec38a8fc15b7
liyao0630 <liyao0630@outlook.com>
2019-11-13 18:49:01 +0800

    up

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const utils = require('./lib/utils')
 const config = require('./config')
 const mixinPvr = require('./lib/mixinPvr')
 const mixinPng = require('./lib/mixinPng')
+const v8 = require('v8')
 
 let filesPath = utils.getFiles(config.entry)
 
@@ -11,12 +12,23 @@ let pvrCount = 0
 function pvrCallback(res) {
   console.log(`共 ${pvrTotal} pvr文件,已处理${++pvrCount}\r\n`)
 }
-mixinPvr(pvrFile, pvrCallback)
+// mixinPvr(pvrFile, pvrCallback)
 
 let pngFile = utils.filterFile(filesPath, '.png')
+pngFile = pngFile.slice(5134, 8406)
 let pngTotal = pngFile.length
 let pngCount = 0
 function pngCallback() {
   console.log(`共 ${pngTotal} png文件,已处理${++pngCount}\r\n`)
 }
+
 mixinPng(pngFile, pngCallback)
+
+
+
+// /Users/ly/Documents/8.sdf/Res_11.12/images/ui/critRoulette/bg.png
+// console.log('进程', process.memoryUsage());
+
+// console.log('堆', v8.getHeapStatistics());
+
+// console.log('内存', v8.getHeapSpaceStatistics());
